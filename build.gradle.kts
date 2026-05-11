e4824548b291b26c947b617dc3f41b2e6f8c9615
Lastelle DUPONT <96951725+drownedNonsense@users.noreply.github.com>
2026-05-11 14:07:44 +0200

    Added SonarQube to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,12 @@ plugins {
     alias(libs.plugins.composeMultiplatform) apply false
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
+    id("org.sonarqube") version "7.3.0.8198"
+}
+
+sonar {
+  properties {
+    property("sonar.projectKey", "CDA-2025-CESI-Zen_CESI-Zen-Mobile")
+    property("sonar.organization", "cda-2025-cesi-zen")
+  }
 }
